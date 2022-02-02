302ea9e96adf81cfb4207be599ae20e68476fa4b
Nicolas <nicoeana.azul@gmail.com>
2022-02-02 17:14:27 -0300

    Arrumando pastas  e resetando angular

--- Frontend/join-example/src/app/app.module.ts
+++ Frontend/join-example/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PessoasComponent } from './pessoas/pessoas.component';
+import { BodyComponent } from './body/body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PessoasComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule,
